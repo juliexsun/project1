fb1615d2fa8302ee297035bbdf0848f59a08e678
juliexsun <152749775+juliexsun@users.noreply.github.com>
2024-02-10 12:38:14 -0500

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -75,32 +75,16 @@ if __name__ == "__main__":
     # testing loading methods
     with open('map.txt') as map_file, open('locations.txt') as location_file, open('items.txt') as item_file:
         w = World(map_file, location_file, item_file)
-        # print(w.map)
-        # for item in w.items:
-        #     print(item.start, item.target, item.target_points, item.name)
         map_list = w.map
         locations_list = w.locations
         items_list = w.items
 
-        # location_object = w.get_location(0, 0)
-        # print(location_object.location_num, location_object.num)
-        # print(location_object.short_description)
-        # print(location_object.long_description)
-
-        # location_object.items = location_object.available_items(items_list)
-        # for item in location_object.items:
-        #     print(item.start, item.target, item.target_points, item.name)
-
-        # map_object = Map(2, 10, 1, 'Bob')
-        # print(map_object.start, map_object.target, map_object.target_points, map_object.name)
-        # map_object.print_map(map_list)
-
     p = Player(0, 0, w)  # set starting location of player; you may change the x, y coordinates here as appropriate
-    # p.use_item('map', Map(1, 1, 1, 'map'), map_list)
     menu = ["look", "inventory", "score", "quit", "resume"]
     max_moves = 10
     current_moves = 0
     score = 0
+    previous_location = '-1-1'
 
     while not p.victory:
         location = w.get_location(p.x, p.y)
@@ -110,44 +94,20 @@ if __name__ == "__main__":
         location.items = location.available_items(items_list)
         score += locations_list[location.location_num].available_score
 
-        if current_moves == max_moves - 1:
-            print("!Warning: You have only one move left!")
-        if current_moves > max_moves:
-            print("The maximum number of moves is reached. You missed your exam! Try again :)")
-            exit()
+        if current_moves == max_moves - 5:
+            print("!Warning: You have only five moves left!")
 
         print("moves: ", current_moves, "/", max_moves)
-
-        # TESTING AVAILABLE ITEMS
-        # location.items = location.available_items(items_list)
-        # print("AVAILABLE ITEMS")
-        # for item in location.items:
-        #     print(item.start, item.target, item.target_points, item.name)
-        # print()
-
-        # print("ALL ITEMS")
-        # for item in items_list:
-        #     print(item.start, item.target, item.target_points, item.name)
-        # print()
 
         # TODO: ENTER CODE HERE TO PRINT LOCATION DESCRIPTION
         # Depending on whether or not it's been visited before,
         # print either full description (first time visit) or brief description (every subsequent visit)
-        print_location_description(location)
-        locations_list[location.location_num].available_score = 0
-        # print("Items at the starting location:", location.items)
-        # p.pick_up_item('Cheat Sheet', location, items_list)
-        # print(items_list)
-        # location1 = w.get_location(0, 0)
-        # p.go_south()
-        # location = w.get_location(p.x, p.y)
-        # print(location.location_num)
-        # print("Player's inventory after picking up:", p.inventory)
-        # p.drop_item('Cheat Sheet', location, items_list)
-        # print(items_list)
-        # print(location1.available_items(items_list))
-        # print("Player's inventory after dropping:", p.inventory)
-        # print("Items at this location:", location.items)
+
+        if previous_location != str(p.x) + str(p.y):
+            print_location_description(location)
+            locations_list[location.location_num].available_score = 0
+        previous_location = str(p.x) + str(p.y)
+
         prompt_direction(location, map_list, p.x, p.y)
         choice = prompt_action(location, items_list, p.inventory)
         while choice.lower() not in location.actions and choice not in location.actions and choice != 'menu':
@@ -172,7 +132,7 @@ if __name__ == "__main__":
             for item in location.items:
                 print(item.name)
             print()
-            item_name = input("Enter the name of the item to pick up: ")
+            item_name = input("Enter the name of the item to pick up exactly formatted as above: ")
             if p.pick_up_item(item_name, location, items_list):
                 for item in p.inventory:
                     score += item.target_points
@@ -183,14 +143,14 @@ if __name__ == "__main__":
             for item in p.inventory:
                 print(item.name)
             print()
-            item_name = input("Enter the name of the item to drop: ")
+            item_name = input("Enter the name of the item to drop exactly formatted as above: ")
             location = w.get_location(p.x, p.y)
             if p.drop_item(item_name, location, items_list):
                 current_moves += 1
         if choice.lower() == 'use map':
             for item in p.inventory:
                 if item.name == 'Map':
-                    p.use_item('Map', item, map_list)
+                    item.print_map(map_list)
         if 'read "How to' in choice:
             for item in items_list:
                 if item.name == choice[5:]:
@@ -213,6 +173,11 @@ if __name__ == "__main__":
 
         location = w.get_location(p.x, p.y)
         p.player_victory(location, score)
+
+        if p.victory is False and current_moves >= max_moves:
+            print("moves: ", current_moves, "/", max_moves)
+            print("The maximum number of moves is reached. You missed your exam! Try again :)")
+            exit()
 
     # TODO: CALL A FUNCTION HERE TO HANDLE WHAT HAPPENS UPON THE PLAYER'S CHOICE
         #  REMEMBER: the location = w.get_location(p.x, p.y) at the top of this loop will update the location if
